6095fb48463fa2e503446a6540642db26958401b
Diaoxiaozhang <zjrdmczh@outlook.com>
2023-02-25 13:33:34 +0800

    添加解析专辑功能

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
+from lxml import etree
+import math
+
+
+headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1660.14"
+    }
 
 
 # 下载单个声音
@@ -8,10 +15,7 @@ def get_sound(sound_id):
         "id": sound_id,
         "ptype": 1
     }
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1660.14"
-    }
-    response = requests.get(url, params=params, headers=headers)
+    response = requests.get(url, headers=headers, params=params)
     sound_url = response.json()["data"]["src"]
     url = "https://www.ximalaya.com/mobile-playpage/track/v3/baseInfo/1677297989848"
     params = {
@@ -25,3 +29,24 @@ def get_sound(sound_id):
     sound_file = response.content
     with open(f"./download/{sound_name}.m4a", mode="wb") as f:
         f.write(sound_file)
+
+
+def analyze_album(album_id):
+    url = f"https://www.ximalaya.com/album/{album_id}"
+    response = requests.get(url, headers=headers)
+    tree = etree.HTML(response.text)
+    episodes = tree.xpath('//*[@id="anchor_sound_list"]/div[1]/span[1]/span/text()[2]')[0]
+    pages = math.ceil(int(episodes) / 100)
+    url = "https://www.ximalaya.com/revision/album/v1/getTracksList"
+    sounds = []
+    for page in range(1, pages + 1):
+        params = {
+            "albumId": album_id,
+            "pageNum": page,
+            "sort": 0,
+            "pageSize": 100
+        }
+        response = requests.get(url, headers=headers, params=params)
+        sounds += response.json()["data"]["tracks"]
+    album_name = sounds[0]["albumTitle"]
+    return album_name, sounds
