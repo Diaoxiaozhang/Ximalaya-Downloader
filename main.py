56e5fdad594f4c6ce5d22e1a00a9071af8afd4ec
Diaoxiaozhang <zjrdmczh@outlook.com>
2023-02-25 14:40:50 +0800

    增加多选声音下载功能

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from lxml import etree
 import math
+import aiohttp
+import aiofiles
+import asyncio
+import os
 
 
 headers = {
@@ -8,8 +12,8 @@ headers = {
     }
 
 
-# 下载单个声音
-def get_sound(sound_id):
+# 解析声音
+def analyze_sound(sound_id):
     url = "https://www.ximalaya.com/revision/play/v1/audio"
     params = {
         "id": sound_id,
@@ -25,10 +29,7 @@ def get_sound(sound_id):
     }
     response = requests.get(url, headers=headers, params=params)
     sound_name = response.json()["trackInfo"]["title"]
-    response = requests.get(sound_url, headers=headers)
-    sound_file = response.content
-    with open(f"./download/{sound_name}.m4a", mode="wb") as f:
-        f.write(sound_file)
+    return sound_name, sound_url
 
 
 # 解析专辑
@@ -51,3 +52,43 @@ def analyze_album(album_id):
         sounds += response.json()["data"]["tracks"]
     album_name = sounds[0]["albumTitle"]
     return album_name, sounds
+
+
+# 将文件名中不能包含的字符替换为空格
+def replace_invalid_chars(name):
+    invalid_chars = ['/', '\\', ':,' '*', '?', '"', '<', '>', '|']
+    for char in invalid_chars:
+        if char in name:
+            name = name.replace(char, " ")
+    return name
+
+
+# 下载单个声音
+def get_sound(sound_name, sound_url):
+    response = requests.get(sound_url, headers=headers)
+    sound_file = response.content
+    with open(f"./download/{sound_name}.m4a", mode="wb") as f:
+        f.write(sound_file)
+
+
+# 协程下载声音
+async def async_get_sound(sound_name, sound_url, album_name, session):
+    sound_name = replace_invalid_chars(sound_name)
+    album_name = replace_invalid_chars(album_name)
+    if not os.path.exists(f"./download/{album_name}"):
+        os.makedirs(f"./download/{album_name}")
+    async with session.get(sound_url, headers=headers) as response:
+        async with aiofiles.open(f"./download/{album_name}/{sound_name}.m4a", mode="wb") as f:
+            await f.write(await response.content.read())
+
+
+# 下载选定专辑声音
+async def get_selected_sounds(sounds, album_name, start, end):
+    tasks = []
+    session = aiohttp.ClientSession()
+    for i in range(start, end + 1):
+        sound_id = sounds[i - 1]["trackId"]
+        sound_name, sound_url = analyze_sound(sound_id)
+        tasks.append(asyncio.create_task(async_get_sound(sound_name, sound_url, album_name, session)))
+    await asyncio.wait(tasks)
+    await session.close()
