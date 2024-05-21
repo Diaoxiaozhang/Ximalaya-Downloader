2684249c5abd85ba4072ad463ee29b62154fdc56
Diaoxiaozhang <zjrdmczh@outlook.com>
2024-05-21 21:53:00 +0800

    debug

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class Ximalaya:
         try:
             not response.json()["trackInfo"]["isAuthorized"]
         except KeyError:
-            print(colorama.Fore.RED + f'ID为{sound_id}的声音解析失败，可能因为达到每日付费音频下载上限！')
+            print(colorama.Fore.RED + f'ID为{sound_id}的声音解析失败，可能因为达到每日音频下载上限！')
             return False
         if not response.json()["trackInfo"]["isAuthorized"]:
             return 0  # 未购买或未登录vip账号
@@ -134,7 +134,7 @@ class Ximalaya:
                     encrypted_url_list = response_json["trackInfo"]["playUrlList"]
                     break
             except KeyError:
-                print(colorama.Fore.RED + f'ID为{sound_id}的声音解析失败，可能因为达到每日付费音频下载上限')
+                print(colorama.Fore.RED + f'ID为{sound_id}的声音解析失败，可能因为达到每日音频下载上限')
                 return False
             except Exception as e:
                 logger.debug(f'ID为{sound_id}的声音解析失败！')
